aa6d383180b5e172146670c5d2da14658114a037
Jessica <jessicaimoesemi@gmail.com>
2025-03-14 15:20:32 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '330f7179-f3fe-416b-8a02-9068658f677b';
+export const MODAL_CLOSE_SEC = 2.5;
